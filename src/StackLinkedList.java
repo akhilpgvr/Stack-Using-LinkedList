800bfbe8541ac6851635c7366ede0e9c833e8919
Akhil <akhilpgvr@gmail.com>
2024-07-12 22:59:02 +0530

    added push and pop operations

--- src/StackLinkedList.java
+++ src/StackLinkedList.java
@@ -1,7 +1,31 @@
+import com.sun.security.jgss.GSSUtil;
+
 public class StackLinkedList {
 
     Node top;
 
 
-    public void
+    public void push(int data) {
+
+        if(top == null) {
+            top = new Node(data);
+        }
+        else {
+            Node newNode = new Node(data);
+            newNode.next = top;
+            top = newNode;
+        }
+        System.out.println("Added element to stack");
+    }
+
+    public void pop() throws Exception {
+
+        if(top == null) {
+            throw new Exception("Stack is Empty");
+        }
+        else{
+            top = top.next;
+            System.out.println("Removed element from stack");
+        }
+    }
 }
